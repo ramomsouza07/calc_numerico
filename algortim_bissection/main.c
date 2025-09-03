ba5008aeeb2edc21ff362acf4e048ed1b591a3e7
RamomSouza <ramomsouza07@gmail.com>
2025-09-03 18:14:36 -0300

    atualização no codigo da bisseção e criação do metodo de newton em python

--- algortim_bissection/main.c
+++ algortim_bissection/main.c
@@ -2,39 +2,36 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*Coeficiente do polinômio*/
-#define c1 1
-#define c2 6
-#define c3 -5
-
 /*Definir cota e número máximo de iteração*/
-#define erro 0.025
+#define erro 0.0001
 #define Nmax 50
 
 /*função f(x)*/
 float f(float x){
-    return (c1 * x * x) + (c2 * x) + c3;
+    return (x * x * x) + ((-4) * x) + 1;
 }
 
 int main() {
-    float b = -6, a = -7;
-
-    if (f(a) * f(b) > 0){
+    /*Intervalo*/
+    float a = 1, b = 2;
+    
+    if (f(a) * f(b) < 0){
+                float y = (a + b)/2;
+        int k = 0;
+        while (fabs(b - a) > erro){
+            k++;
+            y = (a + b)/2;
+            if (f(y) * f(a) > 0){
+                a = y;
+            } else {
+                b = y;
+            }
+            if(k >= Nmax){
+                break;
+            }
+        }
+        printf("O valor aproximado é de: %f\n", y);        
+    }else{
         printf("Não irá convergir\n");
     }
-    float y = (a + b)/2;
-    int k = 0;
-    while (fabs(b - a) > erro){
-        k++;
-        y = (a + b)/2;
-        if (f(y) * f(a) > 0){
-            a = y;
-        } else {
-            b = y;
-        }
-        if(k >= Nmax){
-            break;
-        }
-    }
-    printf("O valor aproximado é de: %f\n", y);
 }
